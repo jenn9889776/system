fb06e2cae67958c2b460eb5b50827f20db3a9798
jenn9889776 <167901322+jenn9889776@users.noreply.github.com>
2024-04-23 09:46:06 -0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,5 @@
+var coo=prompt("Esta pagina puede tener acceso para mandar notificaciones cuando haya una nueva actualisacion o agreguemos informacion nueva a ella, esta de acuerdo con eso?");
+if(coo=="si"){
+
+    alert("Bienvenido")
+}
